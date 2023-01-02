11691d8622404ecf4715285110faea3c1ae9e533
Mücahit Kökdemir <76880536+mucahitdev@users.noreply.github.com>
2023-01-02 21:35:45 +0300

    configs styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,15 @@
-import './globals.css'
+import '@/styles/reset.css'
+import '@/styles/globals.scss'
+
+import { Quicksand } from '@next/font/google'
+const quicksand = Quicksand({ 
+  subsets: ['latin'] ,
+  weights: [400, 700]
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html lang="tr" className={quicksand.className}>
       <head />
       <body>{children}</body>
     </html>
