1e1507d9198ac2a43f3dc17aeb5e3ba37fe9b657
Mücahit Kökdemir <76880536+mucahitdev@users.noreply.github.com>
2023-01-02 20:17:16 +0300

    full version initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,8 @@
 import './globals.css'
-import { Quicksand } from '@next/font/google'
-const quicksand = Quicksand({ subsets: ['latin'] })
-
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={quicksand.className}>
+    <html lang="tr">
       <head />
       <body>{children}</body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,50 +1,7 @@
-import styles from './page.module.css'
-import data from '@/data/user.json'
-import Link from 'next/link'
-
-import Social from '@/components/social'
-
 export default function Home() {
-
-  const { theme, user, other, socials } = data
-  const { title, subtitle, avatar } = user
-
-  let bgColor = theme.background.color
-  let textColor = theme.text.color
-
   return (
-    <main className={styles.main} style={{
-      backgroundColor: bgColor,
-      color: textColor
-    }}>
-      <div className={styles.header}>
-        <img className={styles.avatar} src={avatar.url} alt={title.text} />
-        <p className={styles.title}>
-          {title.text}
-        </p>
-        <p className={styles.subtitle}>
-          {subtitle.text}
-        </p>
-      </div>
-
-      <div className={styles.contents}>
-        {
-          other.map((item, index) => {
-            return (
-              <Link className={styles.content} key={index} href={item.url} target='_blank' style={{
-                borderColor: item.color
-              }}>
-                <p className={styles.contentText} >
-                  {item.title}
-                </p>
-              </Link>
-            )
-          })
-        }
-      </div>
-      <div className={styles.footer}>
-        <Social socials={socials} />
-      </div>
+    <main>
+        Hiii
     </main>
   )
 }
